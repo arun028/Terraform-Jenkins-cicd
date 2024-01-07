573f31932e7a7626ad26b2e8ec2d5b046ac2402c
arun028 <109532495+arun028@users.noreply.github.com>
2024-01-07 12:34:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "test-instance1"
-  }
-}
 resource "aws_instance" "fo" {
   ami           = "ami-079db87dc4c10ac91" # us-west-2
   instance_type = "t2.micro"
