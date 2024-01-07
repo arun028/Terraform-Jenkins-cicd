5367eda26931f079f03dc5709d725175f249ac99
arun028 <109532495+arun028@users.noreply.github.com>
2024-01-07 12:29:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,20 @@ resource "aws_instance" "foo" {
   tags = {
       Name = "test-instance1"
   }
+}
 resource "aws_instance" "fo" {
   ami           = "ami-079db87dc4c10ac91" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "test-int"
   }
-
+}
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1"
+  size = 1
+}
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.fo.id
 }
